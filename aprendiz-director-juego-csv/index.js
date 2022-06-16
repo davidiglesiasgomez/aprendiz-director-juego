cc3e20514759320769225fc7a48060315b4788e6
davidiglesiasgomez <37771705+davidiglesiasgomez@users.noreply.github.com>
2022-06-16 11:13:04 +0200

    Entities en la exportación de las cartas

--- aprendiz-director-juego-csv/index.js
+++ aprendiz-director-juego-csv/index.js
@@ -1,15 +1,22 @@
 const axios = require('axios').default
 const csvWriter = require('csv-writer')
 const path = require('path')
+const { encode } = require('html-entities')
+const numero_cartas = 52
 
 const oracle = async () => {
   const retorno = []
-  for (let i=0; i<=52; i++) {
+  for (let i=1; i<=numero_cartas; i++) {
     try {
-        const response = await axios.get('http://localhost:3001/oracle');
-        // console.log(response.data);
+        const response = await axios.get('http://localhost:3001/oracle', {
+          'headers': {
+            'Accept-language': 'es'
+          }
+        })
+        // console.log('data', response.data)
 
         retorno.push({
+          'number': `${i}/${numero_cartas}`,
           'd4': response.data.d4,
           'd6': response.data.d6,
           'd8': response.data.d8,
@@ -17,10 +24,10 @@ const oracle = async () => {
           'd12': response.data.d12,
           'd20': response.data.d20,
           'd100': response.data.d100,
-          'card': response.data.card,
-          'rune': response.data.rune.rune,
-          'rune_label': response.data.rune.label,
-          'direction': response.data.direction.direction,
+          'card': encode(response.data.card, {mode: 'nonAsciiPrintable', numeric: 'hexadecimal'}),
+          'rune': encode(response.data.rune.rune, {mode: 'nonAsciiPrintable', numeric: 'hexadecimal'}),
+          'rune_label': encode(response.data.rune.label, {mode: 'nonAsciiPrintable', numeric: 'hexadecimal'}),
+          'direction': encode(response.data.direction.direction, {mode: 'nonAsciiPrintable', numeric: 'hexadecimal'}),
           'scene_complication': response.data.scene_complication,
           'altered_scene': response.data.altered_scene.altered_scene || '',
           'oracle_yes_no_likely': response.data.oracle_yes_no_likely,
@@ -29,15 +36,15 @@ const oracle = async () => {
           'oracle_how': response.data.oracle_how,
           'pacing_moves': response.data.pacing_moves.pacing_moves,
           'failure_moves': response.data.failure_moves,
-          'random_event_what_happens': response.data.random_event.what_happens,
-          'random_event_involving': response.data.random_event.involving,
-          'oracle_action_focus': response.data.oracle_action_focus,
-          'oracle_detail_focus': response.data.oracle_detail_focus,
-          'oracle_topic_focus': response.data.oracle_topic_focus,
+          'random_event_what_happens': `${response.data.random_event.what_happens.value}. ${response.data.random_event.what_happens.suit}`,
+          'random_event_involving': `${response.data.random_event.involving.value}. ${response.data.random_event.involving.suit}`,
+          'oracle_action_focus': `${response.data.oracle_action_focus.value}. ${response.data.oracle_action_focus.suit}`,
+          'oracle_detail_focus': `${response.data.oracle_detail_focus.value}. ${response.data.oracle_detail_focus.suit}`,
+          'oracle_topic_focus': `${response.data.oracle_topic_focus.value}. ${response.data.oracle_topic_focus.suit}`,
         })
 
     } catch (err) {
-        console.error(err);
+        console.error('err', err)
 
     }
   }
@@ -51,6 +58,7 @@ cards.then(cards => {
   const writer = csvWriter.createObjectCsvWriter({
     path: path.resolve(__dirname, 'cards.csv'),
     header: [
+      { id: 'number', title: 'number' },
       { id: 'd4', title: 'd4' },
       { id: 'd6', title: 'd6' },
       { id: 'd8', title: 'd8' },
@@ -76,11 +84,11 @@ cards.then(cards => {
       { id: 'oracle_detail_focus', title: 'oracle_detail_focus' },
       { id: 'oracle_topic_focus', title: 'oracle_topic_focus' },
     ],
-  });
+  })
 
   writer.writeRecords(cards).then(() => {
-    console.log('Done!');
-  });
+    console.log('Done!')
+  })
 })
 
 
